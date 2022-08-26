a18ce38ed9dafebab1e542fc3e7469e24bd2bebc
Allen <allenmathews416@gmail.com>
2022-08-27 00:14:54 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById('button');
+
+input.addEventListener('click', function() {
+        alert("binds an event listener in addingEventListener()")
+    });
 }
+addingEventListener(); 
